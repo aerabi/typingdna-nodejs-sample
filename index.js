78f8f41a7ce1c40e8d8f4df7040ffadfbdcf6c60
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-04-08 14:35:35 +0200

    Add CORS and logs

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
 const { TypingDNAReactiveClient } = require('typingdnaclient-rxjs');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 const apiKey = process.env.TYPING_DNA_API_KEY;
 const apiSecret = process.env.TYPING_DNA_API_SECRET;
 const typingDnaClient = new TypingDNAReactiveClient(apiKey, apiSecret);
 
 app.post('/', bodyParser.json(), (req, res) => {
-  typingDnaClient.auto(req.body.user, req.body.pattern).subscribe(autoResponse => res.send(autoResponse));
+  console.log(req.url, req.body);
+  typingDnaClient
+    .auto(req.body.user, req.body.pattern)
+    .subscribe(autoResponse => res.send(autoResponse));
 });
 
 app.listen(port, () => {
